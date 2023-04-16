e45b141bc0c16a9a9bdecd3402f0de8dc8a328d9
grishmav87 <112604862+grishmav87@users.noreply.github.com>
2023-04-16 21:31:25 +0800

    Created search bar component

--- src/components/PubmedSearch.js
+++ src/components/PubmedSearch.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Button } from "react-bootstrap";
 
 export default function PubmedSearch() {
+  let [searchword, setSearchword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${searchword}`);
+  }
+  function handleSearch(event) {
+    setSearchword(event.target.value);
+  }
+
   return (
     <Container>
       <div>Search for a topic</div>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleSearch}></input>
+      </form>
       <Button>Search</Button>
     </Container>
   );
