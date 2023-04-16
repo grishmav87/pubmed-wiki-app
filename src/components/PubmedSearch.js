236149900add6ebc6b28b5761e51ee4d8a4a3edb
grishmav87 <112604862+grishmav87@users.noreply.github.com>
2023-04-16 22:59:19 +0800

    Added apiurl for wikipedia search (included link to documentation)

--- src/components/PubmedSearch.js
+++ src/components/PubmedSearch.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { Container, Button } from "react-bootstrap";
+import axios from "axios";
 
 export default function PubmedSearch() {
   let [searchword, setSearchword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${searchword}`);
+    //documentation for wikiAPI: https://api.wikimedia.org/wiki/API_reference/Core/Search/Search_content
+    let apiUrl = `https://api.wikimedia.org/core/v1/wikipedia/en/search/page?q=${searchword}&limit=10`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleSearch(event) {
     setSearchword(event.target.value);
